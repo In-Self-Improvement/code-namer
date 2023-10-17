9bc7f0e779f627515e461190f80dfcb6039aede4
Bong <borobong230@gmail.com>
2023-10-17 13:27:51 +0900

    feat: Add naming recommend from openai (data type, function, number of list #1

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,92 @@ import "./App.css";
 import OpenAI from "openai";
 import { useState } from "react";
 function App() {
-  const [image, setImage] = useState(null);
+  const [data_type, setDataType] = useState("");
+  const [variable_function, setVariableFunction] = useState("");
+  const [recommendation_number, setRecommendationNumber] = useState(5);
+  const [functionality, setFunctionality] = useState("");
+  const [text, setText] = useState("");
+  const [content, setContent] = useState("");
   const openai = new OpenAI({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY, // This is also the default, can be omitted
     dangerouslyAllowBrowser: true,
   });
 
-  const generateImage = async () => {
-    const image = await openai.images.generate({
-      prompt: "A cute baby sea otter",
+  const handleDataTypeChange = (e) => {
+    setDataType(e.target.value);
+    console.log("data_type", data_type);
+  };
+
+  const handleVariableFunctionChange = (e) => {
+    setVariableFunction(e.target.value);
+    console.log("variable_function", variable_function);
+  };
+
+  const handleRecommendationNumberChange = (e) => {
+    setRecommendationNumber(e.target.value);
+    console.log("recommendation_number", recommendation_number);
+  };
+
+  const handleFunctionality = (e) => {
+    setFunctionality(e.target.value);
+    console.log("functionality", functionality);
+  };
+
+  const assembleContent = (e) => {
+    setContent(
+      `You are a programmer who is good at naming.
+      데이터 타입은 ${data_type}입니다. 
+      기능은 ${functionality} 입니다. 
+      Please list only ${recommendation_number} names.`
+    );
+  };
+
+  const generateText = async (e) => {
+    assembleContent();
+
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content }],
+      model: "gpt-3.5-turbo",
     });
 
-    console.log(image.data);
-    setImage(image.data[0].url);
+    console.log(completion.choices[0].message.content);
+    setText(completion.choices[0].message.content);
   };
   return (
     <div className="App">
-      <button onClick={generateImage}>Generate Image</button>
-      {image && <img src={image} alt={"openai 이미지 생성"} />}
+      <form>
+        <label>
+          데이터 타입
+          <input
+            type="text"
+            name="데이터_타입"
+            onChange={handleDataTypeChange}
+          />
+        </label>
+        <label>
+          함수 | 변수
+          <input
+            type="text"
+            name="변수_함수"
+            onChange={handleVariableFunctionChange}
+          />
+        </label>
+        <label>
+          추천 갯수
+          <input
+            type="text"
+            name="추천_개수"
+            onChange={handleRecommendationNumberChange}
+          />
+        </label>
+        <label>
+          기능
+          <input type="text" name="기능" onChange={handleFunctionality} />
+        </label>
+      </form>
+      <button onClick={generateText}>문자 생성</button>
+      <br />
+      {text && <p>{text}</p>}
     </div>
   );
 }
