a5c96629ba4bafa090d0285b134ac63c30f1ba2b
Bong <borobong230@gmail.com>
2023-11-09 19:46:51 +0900

    feat: 로그인 페이지 모달형태로 변경 close #80

--- src/components/UserStatus/UserStatus.tsx
+++ src/components/UserStatus/UserStatus.tsx
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
 import "./UserStatus.css";
-import SignInButton from "~/components/signin/SignInButton/SignInButton";
-import SignOutButton from "~/components/signin/SignOutButton/SignOutButton";
+import SignInButton from "~/components/UserStatus/SignInButton/SignInButton";
+import SignOutButton from "~/components/UserStatus/SignOutButton/SignOutButton";
 
 import { auth } from "~/firebase/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { SET_LOADING } from "~/redux/slice/loadingSlice";
-
+import SignIn from "~/components/signin/SignIn";
 const UserStatus = () => {
   const [user, loading, error] = useAuthState(auth);
   const dispatch = useDispatch();
   const displayName = user ? user.displayName : null;
   const [clickSignOut, setClickSignOut] = useState(false);
-
+  const [isSignInModalOpen, setIsSignInModalOpen] = useState(false);
   const clickSignInButton = () => {
     if (user) {
       setClickSignOut(!clickSignOut);
     } else {
-      signin();
+      setIsSignInModalOpen(true);
     }
   };
 
-  const signin = () => {
-    // signin 팝업 열기
-    console.log("signin");
+  const closeSignInModal = () => {
+    console.log("close?");
+
+    setIsSignInModalOpen(false);
   };
 
   const signout = () => {
@@ -45,6 +46,7 @@ const UserStatus = () => {
     <div className="userStatus_container">
       <SignInButton displayName={displayName} onClick={clickSignInButton} />
       {clickSignOut && <SignOutButton onClick={signout} />}
+      <SignIn isOpen={isSignInModalOpen} onRequestClose={closeSignInModal} />
     </div>
   );
 };
