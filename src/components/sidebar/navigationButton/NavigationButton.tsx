c91a203d599a9d0c3fc5647f843fad97304a23b0
Bong <borobong230@gmail.com>
2023-11-15 21:44:13 +0900

    feat: 새로운 이름 추천 버튼 눌렀을 때 generateRecommendName 페이지로 이동되는 기능 추가

--- src/components/sidebar/navigationButton/NavigationButton.tsx
+++ src/components/sidebar/navigationButton/NavigationButton.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './NavigationButton.css';
 
 const NavigationButton = () => {
-  const onClickButton = () => {};
+  const onClickButton = () => {
+    window.location.href = '/';
+  };
   return (
     <div className="navigation_button_wrapper">
       <button className="navigation_button" onClick={onClickButton}>
